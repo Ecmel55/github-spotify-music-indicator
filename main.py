2037b2b55a28adf24f94db33bb8c0cdd1eecdaca
Kerem <52210913+Kerem00@users.noreply.github.com>
2023-04-17 03:17:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ current_bio = requests.get("https://api.github.com/user", headers=github_headers
 while True:
     current_track = requests.get("https://api.spotify.com/v1/me/player/currently-playing", headers=spotify_headers, params={"market": "TR"})
     if current_track.status_code == 204:
-        requests.patch("https://api.github.com/user", headers=github_headers, data=f"{json.dumps({'bio': current_bio})}")
+        requests.patch("https://api.github.com/user", headers=github_headers, data=json.dumps({"bio": current_bio}))
         time.sleep(5)
         continue
     elif current_track.status_code == 401:
@@ -39,7 +39,7 @@ while True:
     current_track = current_track.json()
     if current_track["is_playing"]:
         song_name = f"{current_track['item']['name']} ({', '.join([artist['name'] for artist in current_track['item']['artists']])})"
-        requests.patch("https://api.github.com/user", headers=github_headers, data=f"{json.dumps({'bio': f'Now listening: {song_name}'})}")
+        requests.patch("https://api.github.com/user", headers=github_headers, data=json.dumps({'bio': f'Now listening: {song_name}'}))
     else:   
-        requests.patch("https://api.github.com/user", headers=github_headers, data=f"{json.dumps({'bio': current_bio})}")
+        requests.patch("https://api.github.com/user", headers=github_headers, data=json.dumps({"bio": current_bio}))
     time.sleep(5)
